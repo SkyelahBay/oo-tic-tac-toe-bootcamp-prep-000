44eeecf0826ad5d7b767c6b924f34373bdd47872
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-05-08 21:17:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,8 +4,12 @@ def initialize(board=nil)
 end
 
 def display_board
-  puts "#{@board[0]} | #{@board[0]} | #{@board[0]}"
+  puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
   puts "-----------"
+  puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+  puts "-----------"
+  puts "#{@board[6]} | #{@board[7]} | #{@board[9]}"
+end
 
 def WIN_COMBINATIONS
   
