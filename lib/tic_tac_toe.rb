f8da6b2fae551c49aa59de7c8696a08cdb68f1f0
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-05-08 23:13:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -110,7 +110,7 @@ def play
   end
   
   if won? #if over, check for a winner
-    return "Congratulations #{@winner}!"
+    puts "Congratulations #{@winner}!"
   elsif draw?
     puts "Cat's Game!"
   end
