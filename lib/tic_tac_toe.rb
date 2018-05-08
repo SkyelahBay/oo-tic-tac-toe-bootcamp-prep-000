f36ff9e50a61222ec59827a61051281c3ae9cb6b
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-05-08 22:34:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,7 +50,14 @@ def won?
 end
 
 def winner
-  
+  combination = won? #if there is a winner it will return the combination
+  if !over? #if there is no draw or no winner the game is not over yet
+    return nil 
+  elsif @board[combination[0]] == "X" 
+    return "X"
+  elsif @board[combination[0]] == "O" 
+    return "O"
+  end
 end
 
 def full?
